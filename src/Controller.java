6aed47671e6b8d250fb0b145d45e7b32ce31cbef
School <school@Lucas-MacBook-Pro.local>
2019-09-12 16:46:11 -0700

    coord class

--- src/Controller.java
+++ src/Controller.java
@@ -4,8 +4,7 @@ public class Controller {
     char[][] board = new char[12][12]; // c = player, f = fence, e = Mho
     mHos[][] mHos = new mHos[10][10];
 
-    int playerCol;
-    int playerRow;
+    Coord playerLoc = new Coord(0, 0);
 
     int moves = 0;
     int mhosLeft = 12;
---
+++ src/Coord.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Coord {
+    int x; // col
+    int y; // row
+
+    Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
