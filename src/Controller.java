f25ca688d02d3d6f6bb8cafe311422c9769df3ef
School <school@Lucas-MacBook-Pro.local>
2019-09-12 16:55:35 -0700

    coord class

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,7 @@
 package src;
 
 public class Controller {
-    char[][] board = new char[12][12]; // c = player, f = fence, e = Mho
-    mHos[][] mHos = new mHos[10][10];
+    char[][] board = new char[12][12]; // c = player, f = fence, e = Mho, 'v' = empty
 
     Coord playerLoc = new Coord(0, 0);
 
@@ -10,6 +9,26 @@ public class Controller {
     int mhosLeft = 12;
 
     public void turn() {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public void print() {
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                if (board[i][j] != 'v') {
+                    System.out.print(board[i][j] + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("Mhos left %d" %d mhosLeft);
 
     }
 }
