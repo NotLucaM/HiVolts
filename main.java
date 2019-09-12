b4cd03650d562cfa8a6ea583d09acdd2c992828d
School <school@Lucas-MacBook-Pro.local>
2019-09-12 16:40:11 -0700

    stuff

--- main.java
+++
@@ -1,3 +0,0 @@
-class Controller() {
-    char[][] board = new Char[12][12];
-}
---
+++ src/Controller.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Controller {
+    char[][] board = new char[12][12]; // c = player, f = fence, e = Mho
+    mHos[][] mHos = new mHos[10][10];
+
+    int playerCol;
+    int playerRow;
+
+    int moves = 0;
+    int mhosLeft = 12;
+
+    public void turn() {
+
+    }
+}
